32c15f8578b44b0b4cf03634871d18d18a2bd394
John Zhou <john2001@live.ca>
2021-03-11 21:49:44 -0500

    Add gravity and fix getting only translational velocity

--- Assets/Scripts/RigidbodyController.cs
+++ Assets/Scripts/RigidbodyController.cs
@@ -5,14 +5,25 @@ public class RigidbodyController : MonoBehaviour
     public Rigidbody playerRigidbody;
     public Transform playerTransform;
     public float speed;
+    public float gravity = -9.81f;
 
     void FixedUpdate()
     {
         // Get the inputs
         Vector3 targetVelocity = new Vector3(Input.GetAxis("Vertical"), 0, -Input.GetAxis("Horizontal")) * speed;
-        Vector3 currentVelocity = playerTransform.InverseTransformDirection(playerRigidbody.velocity);
+        Vector3 currentVelocity = getCurrentTranslationalVelocity();
 
         playerRigidbody.AddRelativeForce(targetVelocity - currentVelocity, ForceMode.VelocityChange);
+
+        // add gravity
+        playerRigidbody.AddForce(Vector3.up * gravity, ForceMode.Acceleration);
+    }
+
+    Vector3 getCurrentTranslationalVelocity()
+    {
+        Vector3 currentRelativeVelocity = playerTransform.InverseTransformDirection(playerRigidbody.velocity);
+        currentRelativeVelocity.y = 0;
+        return currentRelativeVelocity;
     }
 
 }
