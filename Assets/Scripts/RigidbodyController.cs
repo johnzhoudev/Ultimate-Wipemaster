25817002200d3fd98f105c4b28cf2a059cb592fe
John Zhou <john2001@live.ca>
2021-03-11 20:04:32 -0500

    Add rigidbody controller wasd movement functionality

--- Assets/Scripts/RigidbodyController.cs
+++ Assets/Scripts/RigidbodyController.cs
@@ -2,5 +2,17 @@
 
 public class RigidbodyController : MonoBehaviour
 {
-    
+    public Rigidbody playerRigidbody;
+    public Transform playerTransform;
+    public float speed;
+
+    void FixedUpdate()
+    {
+        // Get the inputs
+        Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * speed;
+        Vector3 currentVelocity = playerTransform.InverseTransformDirection(playerRigidbody.velocity);
+
+        playerRigidbody.AddRelativeForce(targetVelocity - currentVelocity, ForceMode.VelocityChange);
+    }
+
 }
