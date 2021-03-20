d19ee4222aff432b6c458daa6ce747e9b6f06c94
John Zhou <john2001@live.ca>
2021-03-20 17:26:42 -0400

    Add Jump detection

--- Assets/Scripts/RigidbodyController.cs
+++ Assets/Scripts/RigidbodyController.cs
@@ -4,8 +4,17 @@ public class RigidbodyController : MonoBehaviour
 {
     public Rigidbody playerRigidbody;
     public Transform playerTransform;
+    public CapsuleCollider playerCollider;
     public float speed;
     public float gravity = -9.81f;
+
+    public float groundCheckDistance;
+    float distanceToGround;
+
+    private void Start()
+    {
+        distanceToGround = playerCollider.bounds.extents.y;
+    }
 
     void FixedUpdate()
     {
@@ -13,10 +22,18 @@ public class RigidbodyController : MonoBehaviour
         Vector3 targetVelocity = new Vector3(Input.GetAxis("Vertical"), 0, -Input.GetAxis("Horizontal")) * speed;
         Vector3 currentVelocity = getCurrentTranslationalVelocity();
 
+        // Add horizontal movements
         playerRigidbody.AddRelativeForce(targetVelocity - currentVelocity, ForceMode.VelocityChange);
 
         // add gravity
         playerRigidbody.AddForce(Vector3.up * gravity, ForceMode.Acceleration);
+
+        // Jump check: If grounded and spacebar
+        if (Physics.Raycast(playerTransform.position, Vector3.down, distanceToGround + groundCheckDistance) &&
+            Input.GetButton("Jump"))
+        {
+            Debug.Log("Jump Detected");
+        }
     }
 
     Vector3 getCurrentTranslationalVelocity()
