b35c745770bdf4dcdca8a3d49544ab4e57061d7f
John Zhou <john2001@live.ca>
2021-03-24 20:16:32 -0400

    Fix mouse build sensitivity difference by remove time.deltaTime

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -12,19 +12,20 @@ public class MouseLook : MonoBehaviour
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         // Get Mouse x movement and rotate playerBody
-        float mouseX = Input.GetAxis("Mouse X") * mouseXSensitivity * Time.deltaTime;
-        //player.Rotate(Vector3.up * mouseX);
-        playerRigidbody.rotation *= Quaternion.Euler(Vector3.up * mouseX);
+        float mouseX = Input.GetAxis("Mouse X") * mouseXSensitivity;
+        player.Rotate(Vector3.up * mouseX);
+        //playerRigidbody.rotation *= Quaternion.Euler(Vector3.up * mouseX);
 
         // Get Mouse Y movement and rotate camera
         // Easier to just set the rotation instead of checking to see if past 90 degrees or -90 degrees and adjusting accordingly
-        float mouseY = Input.GetAxis("Mouse Y") * mouseYSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseYSensitivity;
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f); // Clamping
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f); // rotate camera instead of player
